ebcf36e7d0bcfc61d8100dff940c9176a7cbcb2f
henryfuentesbenito <127889588+henryfuentesbenito@users.noreply.github.com>
2024-08-26 13:17:11 +0200

    Update encoders.py

--- dnri/models/encoders.py
+++ dnri/models/encoders.py
@@ -7,8 +7,8 @@ import torch.nn.functional as F
 import numpy as np
 from .model_utils import encode_onehot, RefNRIMLP
 
-class BaseEncoder(nn.Module):
 
+class BaseEncoder(nn.Module):
     def __init__(self, num_vars, graph_type):
         super(BaseEncoder, self).__init__()
         self.num_vars = num_vars
@@ -37,7 +37,7 @@ class BaseEncoder(nn.Module):
             incoming = torch.matmul(self.edge2node_mat, tmp_embeddings).view(old_shape[0], -1, old_shape[2], old_shape[3])
         else:
             incoming = torch.matmul(self.edge2node_mat, edge_embeddings)
-        return incoming / (self.num_vars - 1)
+        return incoming / (self.num_vars - 1)  # Averaging incoming messages
 
     def forward(self, inputs, state=None, return_state=False):
         raise NotImplementedError
@@ -45,40 +45,35 @@ class BaseEncoder(nn.Module):
 class RefMLPEncoder(BaseEncoder):
     def __init__(self, params):
         num_vars = params['num_vars']
-        inp_size = params['input_size']
+        inp_size = params['input_size'] * params['input_time_steps']
         hidden_size = params['encoder_hidden']
         num_edges = params['num_edge_types']
-        factor = not params['encoder_no_factor']
         graph_type = params['graph_type']
         super(RefMLPEncoder, self).__init__(num_vars, graph_type)
-        dropout = params['encoder_dropout']
-        self.input_time_steps = params['input_time_steps']
-        self.dynamic = self.graph_type == 'dynamic'
-        self.factor = factor
+        
+        # Transformer Configuration
+        self.transformer = nn.Transformer(
+            d_model=hidden_size, 
+            nhead=params['num_heads'], 
+            num_encoder_layers=params['num_layers'], 
+            num_decoder_layers=params['num_layers'], 
+            dim_feedforward=params['ffn_dim'],
+            dropout=params['encoder_dropout']
+        )
 
-        # LSTM layer
-        self.lstm = nn.LSTM(input_size=inp_size, hidden_size=hidden_size, batch_first=True)
-
-        if self.factor:
-            self.mlp4 = RefNRIMLP(hidden_size * 3, hidden_size, hidden_size, dropout, no_bn=False)
-            print("Using factor graph LSTM encoder.")
-        else:
-            self.mlp4 = RefNRIMLP(hidden_size * 2, hidden_size, hidden_size, dropout, no_bn=False)
-            print("Using LSTM encoder.")
-
-        num_layers = params['encoder_mlp_num_layers']
-        if num_layers == 1:
-            self.fc_out = nn.Linear(hidden_size, num_edges)
-        else:
-            tmp_hidden_size = params['encoder_mlp_hidden']
-            layers = [nn.Linear(hidden_size, tmp_hidden_size), nn.ELU(inplace=True)]
-            for _ in range(num_layers - 2):
-                layers.append(nn.Linear(tmp_hidden_size, tmp_hidden_size))
-                layers.append(nn.ELU(inplace=True))
-            layers.append(nn.Linear(tmp_hidden_size, num_edges))
-            self.fc_out = nn.Sequential(*layers)
+        self.input_proj = nn.Linear(inp_size, hidden_size)
+        self.fc_out = nn.Linear(hidden_size, num_edges)
 
         self.init_weights()
+
+    def node2edge(self, node_embeddings):
+        send_embed = node_embeddings[:, self.send_edges, :]
+        recv_embed = node_embeddings[:, self.recv_edges, :]
+        return torch.cat([send_embed, recv_embed], dim=2)
+
+    def edge2node(self, edge_embeddings):
+        incoming = torch.matmul(self.edge2node_mat, edge_embeddings)
+        return incoming / (self.num_vars - 1)
 
     def init_weights(self):
         for m in self.modules():
@@ -86,38 +81,25 @@ class RefMLPEncoder(BaseEncoder):
                 nn.init.xavier_normal_(m.weight.data)
                 m.bias.data.fill_(0.1)
 
-    def merge_states(self, states):
-        return torch.cat(states, dim=0)
-
     def forward(self, inputs, state=None, return_state=False):
         if inputs.size(1) > self.input_time_steps:
             inputs = inputs[:, -self.input_time_steps:]
         elif inputs.size(1) < self.input_time_steps:
-            begin_inp = inputs[:, 0:1].expand(-1, self.input_time_steps - inputs.size(1), -1, -1)
+            begin_inp = inputs[:, 0:1].expand(-1, self.input_time_steps-inputs.size(1), -1, -1)
             inputs = torch.cat([begin_inp, inputs], dim=1)
         
         if state is not None:
             inputs = torch.cat([state, inputs], 1)[:, -self.input_time_steps:]
 
-        # Pass through LSTM
-        x, (hidden, cell) = self.lstm(inputs)
+        x = inputs.transpose(1, 2).contiguous().view(inputs.size(0), inputs.size(2), -1)
+        x = self.input_proj(x)  # Project input to the Transformer dimensions
 
-        # Combine node embeddings to form edge embeddings
-        x = self.node2edge(x)
-        x_skip = x
-
-        if self.factor:
-            x = self.edge2node(x)
-            x = torch.cat((x, x_skip), dim=-1)  # Skip connection
-            x = self.mlp4(x)
-        else:
-            x = torch.cat((x, x_skip), dim=-1)  # Skip connection
-            x = self.mlp4(x)
-
+        x = self.transformer(x.permute(1, 0, 2))  # Transformer expects input as (seq_len, batch, feature)
+        x = x.permute(1, 0, 2)  # Permute back to original shape
+        
         result = self.fc_out(x)
-
         result_dict = {
             'logits': result,
-            'state': (hidden, cell) if return_state else None,
+            'state': inputs,
         }
         return result_dict
